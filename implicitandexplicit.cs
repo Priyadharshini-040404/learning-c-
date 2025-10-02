e2a065c84bb97734f8e895654a13567efe3b0566
Priyadharshini-040404 <priyadharshini.arulanantham@psiog.com>
2025-10-02 16:21:12 +0530

    implicitand explicit

---
+++ implicitandexplicit.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        double b = a;
+        double c = 9.8;
+        int d = (int)c;
+        Console.WriteLine(b);  
+        Console.WriteLine(c);
+    }
+}
