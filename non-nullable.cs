fddc3b7b8f74a8c20b2a225bfa269047ffa1240b
Priyadharshini-040404 <priyadharshini.arulanantham@psiog.com>
2025-10-02 13:36:54 +0530

    non null

---
+++ non-nullable.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int age = 25;  
+        bool isActive = true; 
+
+        Console.WriteLine($"Age: {age}, Active: {isActive}");
+    }
+}
