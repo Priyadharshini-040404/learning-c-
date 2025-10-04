bc023d7cf8a9f5273eab0298c9897d2e90b5a323
Priyadharshini Arulanantham <priyadharshini.arulanantham@psiog.com>
2025-10-04 17:50:16 +0530

    serverconnection

---
+++ sqlserverconnection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Server=PSILENL068;Database=Chinook;Integrated Security=True;";
+        string query = "SELECT TOP 10 * FROM Artist";
+
+        using (SqlConnection conn = new SqlConnection(connectionString))
+        {
+            try
+            {
+                conn.Open();
+                Console.WriteLine(" Connection successful!");
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    Console.WriteLine("Columns in result:");
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        Console.Write(reader.GetName(i) + "\t");
+                    }
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write(reader.GetValue(i) + "\t");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("SQL Error: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
